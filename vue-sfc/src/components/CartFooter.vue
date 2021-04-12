3176d6b58fb1e704273a0420006bb594f32c5eb9
남승균 <skyun.nam@gmail.com>
2021-04-12 11:39:46 +0900

    Single File Components

---
+++ vue-sfc/src/components/CartFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>&copy; Bit Academy 2021</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    p {
+        font-style: italic;
+        font-weight: bold;
+    }
+</style>
---
+++ vue-sfc/src/components/ShopList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>살 물건들</h2>
+        <ul>
+            <li v-for='(item, index) in shoppingItems' v-if="!item.buy">
+                {{ item.name }}
+                <button v-on:click="buyItem(item, index)">구매</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['shopping-items'],
+    methods: {
+        buyItem: function(item, index) {
+            console.log(item);
+            item.buy = true;
+        }
+    }
+}
+</script>
